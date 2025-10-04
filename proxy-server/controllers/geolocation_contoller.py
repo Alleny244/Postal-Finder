7e8e791d1a2df419b4b648d18ca86ec6b5b8ece6
Alleny244 <alleny244@gmail.com>
2025-10-05 00:01:22 +0530

    feat: Adds javascipt module for finding geolocation and pincode

--- proxy-server/controllers/geolocation_contoller.py
+++ proxy-server/controllers/geolocation_contoller.py
@@ -5,11 +5,10 @@ from settings import URL, API_KEY
 
 
 class GeolocationController:
-    async def reverse_geocode(lat: str, lng: str) -> dict:
+    async def reverse_geocode(self,lat: str, lng: str) -> dict:
         url = f"{URL}?latlng={lat},{lng}&api_key={API_KEY}"
         async with httpx.AsyncClient() as client:
             response = await client.get(url)
             if response.status_code != 200:
                 raise HTTPException(status_code=500, detail="Failed to fetch server")
-            data = response.json()
-            return data
+            return response
